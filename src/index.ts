ca8e6f9f808e4ca766c811d10c4164e4c0c59445
aditya172926 <aditya26sg@gmail.com>
2026-01-31 14:52:26 +0530

    fetching all addresses from contracts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
-import { Contract, ethers, Interface, JsonRpcProvider, parseUnits, TransactionReceipt, Wallet } from "ethers";
+import { Contract, ContractTransactionReceipt, ContractTransactionResponse, Interface, isAddress, JsonRpcProvider, Wallet } from "ethers";
 import BTCeAbi from "./abis/BTCe.json";
 import ERC20Abi from "./abis/ERC20.json";
+import LBTCvAbi from "./abis/LBTCv.json";
 import LAccountantAbi from "./abis/Lombard_Accountant.json";
-import { LOMBARD_ACCOUNTANT_CONTRACT_ADDRESS, LOMBARD_BTCE_CONTRACT_ADDRESS, WBTC_TOKEN_ADDRESS } from "./constants";
+import TellerAbi from "./abis/Teller.json";
+import { LOMBARD_BTCE_CONTRACT_ADDRESS } from "./constants";
 import { EVMContract } from "./interface";
 
 enum TransactionStatus {
@@ -23,11 +25,55 @@ function setup_wallet(): Wallet {
     return wallet;
 }
 
-function initialize_contracts(wallet: Wallet): EVMContract {
+async function callContractMethod<T>(
+    contract: Contract,
+    methodName: string,
+    ...args: any[]
+): Promise<T> {
+    const fn = contract[methodName];
+    if (!fn) throw new Error(`Function ${methodName} not found on contract`);
+    return await fn(...args);
+}
+
+async function sendContractMethod(
+    contract: Contract,
+    methodName: string,
+    ...args: any[]
+): Promise<ContractTransactionReceipt> {
+    const fn = contract[methodName];
+    if (!fn) throw new Error(`Function ${methodName} not found on contract`);
+
+    const txn: ContractTransactionResponse = await fn(...args);
+    const receipt = await txn.wait();
+    if (!receipt) throw new Error(`Failed to get transaction receipt ${txn.hash}`);
+
+    return receipt;
+}
+
+async function initialize_contracts(wallet: Wallet, tokenToDeposit: string): Promise<EVMContract> {
+    const btceContract = new Contract(LOMBARD_BTCE_CONTRACT_ADDRESS, BTCeAbi, wallet);
+
+    const tellerAddress = await callContractMethod<string>(btceContract, "teller");
+    const tellerContract = new Contract(tellerAddress, TellerAbi, wallet);
+
+    const lbtcvAddress: string = await callContractMethod<string>(tellerContract, "vault");
+    const lbtcvContract = new Contract(lbtcvAddress, LBTCvAbi, wallet);
+
+    const accountantAddress = await callContractMethod<string>(tellerContract, "accountant");
+    const accountantContract = new Contract(accountantAddress, LAccountantAbi, wallet);
+
+    const baseTokenAddress = await callContractMethod<string>(accountantContract, "base");
+    const baseTokenContract = new Contract(baseTokenAddress, ERC20Abi, wallet);
+
+    const depositTokenContract = new Contract(tokenToDeposit, ERC20Abi, wallet);
+
     return {
-        btce: new Contract(LOMBARD_BTCE_CONTRACT_ADDRESS, BTCeAbi, wallet),
-        wbtc: new Contract(WBTC_TOKEN_ADDRESS, ERC20Abi, wallet),
-        accountant: new Contract(LOMBARD_ACCOUNTANT_CONTRACT_ADDRESS, LAccountantAbi, wallet)
+        btce: { address: LOMBARD_BTCE_CONTRACT_ADDRESS, contract: btceContract },
+        lombardVault: { address: lbtcvAddress, contract: lbtcvContract },
+        teller: { address: tellerAddress, contract: tellerContract },
+        baseToken: { address: baseTokenAddress, contract: baseTokenContract },
+        depositToken: { address: tokenToDeposit, contract: depositTokenContract },
+        accountant: { address: accountantAddress, contract: accountantContract }
     }
 }
 
@@ -38,19 +84,14 @@ function calculateApy(currentRate: bigint, pastRate: bigint): number {
     return apy * 100;
 }
 
-async function getVaultDetails(contracts: EVMContract, wallet: Wallet) {
-    const baseTokenAddressFn = contracts.accountant["base"];
-    if (!baseTokenAddressFn) throw new Error("Base token address function interface not found");
-    const baseTokenAddress = await baseTokenAddressFn();
-    
-    const baseTokenContract = new Contract(baseTokenAddress, ERC20Abi, wallet);
-    const decimalsFn = baseTokenContract["decimals"];
-    if (!decimalsFn) throw new Error("Token Decimals function interface not found");
-    const decimals = await decimalsFn();
+function calculateTVL() {
 
-    const currentShareRateFn = contracts.accountant["getRate"];
-    if (!currentShareRateFn) throw new Error("Current share rate function interface not found");
-    const currentShareRate: bigint = await currentShareRateFn();
+}
+
+async function getVaultDetails(contracts: EVMContract, wallet: Wallet) {
+    const decimals = await callContractMethod<bigint>(contracts.baseToken.contract, "decimals");
+
+    const currentShareRate: bigint = await callContractMethod<bigint>(contracts.accountant.contract, "getRate");
 
     // get shareRate of 14 days back
     const currentBlockNumber = await wallet.provider?.getBlockNumber();
@@ -62,13 +103,13 @@ async function getVaultDetails(contracts: EVMContract, wallet: Wallet) {
 
     const accountantInterface = new Interface(LAccountantAbi);
     const exchangeRateEventTopic = accountantInterface.getEvent("ExchangeRateUpdated")?.topicHash;
-    if (!exchangeRateEventTopic) throw new Error("Exchange Rate Event Topic hash not found")    ;
+    if (!exchangeRateEventTopic) throw new Error("Exchange Rate Event Topic hash not found");
 
     const logs = await wallet.provider?.getLogs({
         fromBlock: pastBlockNumber,
         toBlock: currentBlockNumber,
         topics: [exchangeRateEventTopic],
-        address: LOMBARD_ACCOUNTANT_CONTRACT_ADDRESS
+        address: contracts.accountant.address
     });
 
     if (logs) {
@@ -87,22 +128,17 @@ async function getVaultDetails(contracts: EVMContract, wallet: Wallet) {
     }
 
     console.log("Current share rate ", currentShareRate);
-    console.log("base token address ", baseTokenAddress);
+    console.log("base token address ", contracts.baseToken.address);
     console.log("base token decimals ", decimals);
 }
 
 async function checkBalance(contract: Contract, userAddress: string): Promise<bigint> {
-    const balanceFn = contract["balanceOf"];
-    if (!balanceFn) throw new Error("Balance check function interface not found");
-    const userBalance: bigint = await balanceFn(userAddress);
+    const userBalance: bigint = await callContractMethod<bigint>(contract, "balanceOf", userAddress);
     return userBalance;
 }
 
 async function approveTokens(contract: Contract, spender: string, amount: number) {
-    const approveFn = contract["approve"];
-    if (!approveFn) throw new Error("Approve Tokens function interface not found");
-    const approveTransaction = await approveFn(spender, amount);
-    const approveTxnReceipt: TransactionReceipt = await approveTransaction.wait();
+    const approveTxnReceipt: ContractTransactionReceipt = await sendContractMethod(contract, "approve", spender, amount);
     if (approveTxnReceipt.status === TransactionStatus.SUCCESS) {
         console.log("Token Approval success");
     } else {
@@ -111,51 +147,45 @@ async function approveTokens(contract: Contract, spender: string, amount: number
 }
 
 async function depositTokens(contract: Contract, tokenAddress: string, amount: number, receiver: string, minShareAmt: number, nonce: number): Promise<boolean> {
-    const depositFn = contract["deposit(address,uint256,address,uint256)"];
-    if (!depositFn) throw new Error("Deposit function interface not found");
-    const depositTransaction = await depositFn(tokenAddress, amount, receiver, minShareAmt, { nonce });
-    const depositTxnReceipt: TransactionReceipt = await depositTransaction.wait();
+    const depositTxnReceipt: ContractTransactionReceipt = await sendContractMethod(contract, "deposit(address,uint256,address,uint256)", tokenAddress, amount, receiver, minShareAmt, { nonce });
     return depositTxnReceipt.status == TransactionStatus.SUCCESS;
 }
 
 async function withdrawTokens(contract: Contract, amount: bigint, receiver: string, owner: string): Promise<boolean> {
-    const withdrawFn = contract["withdraw"];
-    if (!withdrawFn) throw new Error("Withdraw tokens interface not found");
-    const withdrawTransaciton = await withdrawFn(amount, receiver, owner);
-    const withdrawTxnReceipt: TransactionReceipt = await withdrawTransaciton.wait();
+    const withdrawTxnReceipt: ContractTransactionReceipt = await sendContractMethod(contract, "withdraw", amount, receiver, owner)
     return withdrawTxnReceipt.status == TransactionStatus.SUCCESS;
 }
 
-async function main() {
+async function main(tokenToDeposit: string) {
     try {
         const wallet: Wallet = setup_wallet();
         const userAddress: string = await wallet.getAddress();
-        const contracts: EVMContract = initialize_contracts(wallet);
+        const contracts: EVMContract = await initialize_contracts(wallet, tokenToDeposit);
 
         await getVaultDetails(contracts, wallet);
 
         // balance before deposit
-        console.log("Starting User Balance BTCe ", await checkBalance(contracts.btce, userAddress));
+        console.log("Starting User Balance BTCe ", await checkBalance(contracts.btce.contract, userAddress));
 
         // token approval
-        await approveTokens(contracts.wbtc, LOMBARD_BTCE_CONTRACT_ADDRESS, 1000);
+        await approveTokens(contracts.depositToken.contract, contracts.btce.address, 1000);
 
         // get latest nonce
         const nonce = await wallet.getNonce();
         console.log("sending deposit with nonce ", nonce);
 
         // deposit function call
-        const depositSuccess = await depositTokens(contracts.btce, WBTC_TOKEN_ADDRESS, 1000, userAddress, 0, nonce);
+        const depositSuccess = await depositTokens(contracts.btce.contract, contracts.depositToken.address, 1000, userAddress, 0, nonce);
         if (depositSuccess) {
-            console.log("User BTCe Balance after deposit ", await checkBalance(contracts.btce, userAddress));
+            console.log("User BTCe Balance after deposit ", await checkBalance(contracts.btce.contract, userAddress));
         } else {
             throw new Error(`Deposit Transaction failed`);
         }
 
         // withdraw flow
-        const withdrawSuccess = await withdrawTokens(contracts.btce, BigInt(1000), userAddress, userAddress);
+        const withdrawSuccess = await withdrawTokens(contracts.btce.contract, BigInt(1000), userAddress, userAddress);
         if (withdrawSuccess) {
-            console.log("User BTCe Balance after withdraw ", await checkBalance(contracts.btce, userAddress));
+            console.log("User BTCe Balance after withdraw ", await checkBalance(contracts.btce.contract, userAddress));
         } else {
             throw new Error(`Withdraw Transaction failed`);
         }
@@ -165,4 +195,14 @@ async function main() {
     }
 }
 
-main();
+const tokenToDeposit = process.argv[2];
+if (!tokenToDeposit) {
+    console.error("Usage: npm run start -- <tokenAddress>");
+    process.exit(1);
+}
+
+if (!isAddress(tokenToDeposit)) {
+    throw new Error("Invalid token address");
+}
+
+main(tokenToDeposit);
--- src/interface.ts
+++ src/interface.ts
@@ -1,7 +1,15 @@
 import { Contract } from "ethers"
 
 export interface EVMContract {
-    btce: Contract,
-    wbtc: Contract,
-    accountant: Contract
+    btce: EVMData,
+    lombardVault: EVMData,
+    teller: EVMData,
+    baseToken: EVMData,
+    depositToken: EVMData,
+    accountant: EVMData
+}
+
+export interface EVMData {
+    address: string,
+    contract: Contract
 }
